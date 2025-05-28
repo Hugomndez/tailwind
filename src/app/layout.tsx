66845ed1a2618f302f81b644d99054c635da00d6
Hugo Méndez <hello@hugomendez.dev>
2025-05-27 20:33:21 -0500

    feat: refactor font structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { geistMono, geistSans } from './_fonts';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
